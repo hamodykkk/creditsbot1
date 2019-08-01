905f26c58f7a5e3848cd49480405cb56087171fb
hamodykkk <43255826+hamodykkk@users.noreply.github.com>
2019-08-02 01:19:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,6 +81,33 @@ if (message.content === '!spam') {
       }
 });
 
+client.on('message', message => { 
+var prefix = "+"; 
+  if (message.author.bot) return; 
+  if (!message.content.startsWith(prefix)) return; 
+    var argresult = message.content.split(` `).slice(1).join(' '); 
+      if (!devs.includes(message.author.id)) return; 
+  let command = message.content.split(" ")[0]; 
+  command = command.slice(prefix.length); 
+       
+  let args = message.content.split(" ").slice(1);  
+       
+  if (command === "say")  { 
+  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **'); 
+          message.delete() 
+    message.channel.sendMessage(args.join(" ")).catch(console.error); 
+  }         
+     
+if (command == "emb")    { 
+  if(!message.channel.guild) return message.reply('** __This command only for servers⛔__  **'); 
+    let say = new Discord.RichEmbed() 
+    .setDescription(args.join("  ")) 
+    .setColor("RANDOM") 
+    message.channel.sendEmbed(say); 
+    message.delete(); 
+  }  
+});
+
 
 
 
